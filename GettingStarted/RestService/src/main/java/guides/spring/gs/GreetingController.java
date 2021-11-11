82c4aee4d0885df34127fc9ac9f2d21d2ba9dfa6
Stanley Wintergreen <kepocnhh@gmail.com>
2021-11-11 12:23:23 +0300

    Getting Started / Rest Service / init project maven

---
+++ GettingStarted/RestService/src/main/java/guides/spring/gs/GreetingController.java
@@ -0,0 +1,19 @@
+package guides.spring.gs;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+	private static final String template = "Hello, %s!";
+	private static final AtomicLong counter = new AtomicLong();
+
+	@RequestMapping(method = RequestMethod.GET, value = "/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+}
