d04212613bea4f132a5b5b217994952666fffffb
Stanley Wintergreen <kepocnhh@gmail.com>
2021-11-19 22:37:08 +0300

    Getting Started / Authenticating User / init project java17

---
+++ GettingStarted/AuthenticatingUser/java17/src/main/java/guides/spring/gs/App.java
@@ -0,0 +1,11 @@
+package guides.spring.gs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(final String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
